fde89641ce8eeb8c6002e6fed1bf0eeb87d4534d
Sampe <samuel.sandenas123@gmail.com>
2024-03-10 23:34:48 +0100

    Lite grunder bara

--- src/main.js
+++ src/main.js
@@ -1,14 +1,38 @@
 let app = Vue.createApp({
-    data() {
-
+  data() {
+    return {
+      newExpense: {
+        price: 0,
+        date: "",
+        desc: "",
+        category: "",
+      },
+      expenses: [],
+    };
+  },
+  computed: {},
+  methods: {
+    addExpense() {
+      if (
+        this.newExpense.category !== "" &&
+        this.newExpense.price > 0 &&
+        this.newExpense.date !== ""
+      ) {
+        this.expenses.push({
+          price: this.newExpense.price,
+          date: this.newExpense.date,
+          desc: this.newExpense.desc,
+          category: this.newExpense.category,
+        });
+        this.newExpense.price = 0;
+        this.newExpense.date = "";
+        this.newExpense.desc = "";
+        this.newExpense.category = "";
+      } else {
+        alert("Ej korrekt input");
+      }
     },
-    computed: {
+  },
+});
 
-    },
-    methods: {
-
-    },
-
-})
-
-app.mount("#app")
+app.mount("#app");
