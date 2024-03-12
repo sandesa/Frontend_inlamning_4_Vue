1ce5f38e25ebd7b46c6088a9ddbdbe70ad989927
Sampe <samuel.sandenas123@gmail.com>
2024-03-12 02:11:31 +0100

    Lagt till baslogiken

--- src/main.js
+++ src/main.js
@@ -15,18 +15,18 @@ let app = Vue.createApp({
         desc: "",
         category: "",
       },
-      totalExpenses: [],
+      totalExpenses: {
+        all: [],
+        bostad: [],
+        hushåll: [],
+        matOchDryck: [],
+        transport: [],
+        nöjeOchShopping: [],
+        övrigt: [],
+      },
     };
   },
-  computed: {
-    totalPrice: function () {
-      let sum = 0;
-      this.totalExpenses.forEach((exp) => {
-        sum += exp.price;
-      });
-      return sum;
-    },
-  },
+  computed: {},
   methods: {
     addExpense() {
       if (
@@ -34,7 +34,20 @@ let app = Vue.createApp({
         this.newExpense.price > 0 &&
         this.newExpense.date !== ""
       ) {
-        this.totalExpenses.push({
+        let temp = this.newExpense.category;
+        if (temp === this.categories[2]) {
+          temp = "matOchDryck";
+        } else if (temp === this.categories[4]) {
+          temp = "nöjeOchShopping";
+        }
+        temp = temp[0].toLowerCase() + temp.slice(1);
+        this.totalExpenses[temp].push({
+          price: this.newExpense.price,
+          date: this.newExpense.date,
+          desc: this.newExpense.desc,
+          category: this.newExpense.category,
+        });
+        this.totalExpenses.all.push({
           price: this.newExpense.price,
           date: this.newExpense.date,
           desc: this.newExpense.desc,
@@ -48,6 +61,73 @@ let app = Vue.createApp({
         alert("Ej korrekt input");
       }
     },
+  },
+});
+
+app.component("result-table", {
+  props: ["categories", "totalExp", "selectedCategory"],
+  template: `
+    <div>
+      <label for="selectCategory">Select a category:</label>
+      <select id="selectCategory" v-model="selectedCategory">
+        <option value="">None</option>
+        <option value="all">All</option>
+        <option
+          v-for="(category, index) in categories"
+          :key="index"
+          :value="category"
+        >
+          {{category}}
+        </option>
+      </select>
+
+      <table v-if="selectedCategory !== ''">
+        <tr>
+          <th>Kostnad</th>
+          <th>Datum</th>
+          <th>Kategori</th>
+          <th>Beskrivning</th>
+        </tr>
+        <tr
+          v-for="(exp, index) in totalExp[formattedCategory]"
+          :key="index"
+        >
+          <td>{{exp.price}}</td>
+          <td>{{exp.date}}</td>
+          <td>{{exp.category}}</td>
+          <td>{{exp.desc}}</td>
+        </tr>
+        <tr>
+          <td>Summa:</td>
+          <td>{{sum}}</td>
+        </tr>
+      </table>
+    </div>
+  `,
+  computed: {
+    formattedCategory() {
+      let temp = this.selectedCategory;
+      if (temp === this.categories[2]) {
+        temp = "matOchDryck";
+      } else if (temp === this.categories[4]) {
+        temp = "nöjeOchShopping";
+      }
+      return temp[0].toLowerCase() + temp.slice(1);
+    },
+    sum() {
+      let sum = 0;
+      if (this.formattedCategory !== "") {
+        this.totalExp[this.formattedCategory].forEach((exp) => {
+          sum += exp.price;
+        });
+        return sum;
+      }
+    },
+  },
+  data() {
+    return {
+      selectedCategory: "",
+    };
   },
 });
 
