54ba67dc476fc65973e3be597fb819444dcb9403
Sampe <samuel.sandenas123@gmail.com>
2024-03-12 17:50:47 +0100

    Ändrat logiken och lade till delete btn Tyckte logiken och strukturen var lite för luddig och oklar så gjorde om en hel del.

--- src/main.js
+++ src/main.js
@@ -15,15 +15,7 @@ let app = Vue.createApp({
         desc: "",
         category: "",
       },
-      totalExpenses: {
-        all: [],
-        bostad: [],
-        hushåll: [],
-        matOchDryck: [],
-        transport: [],
-        nöjeOchShopping: [],
-        övrigt: [],
-      },
+      expenses: [],
     };
   },
   computed: {},
@@ -34,20 +26,7 @@ let app = Vue.createApp({
         this.newExpense.price > 0 &&
         this.newExpense.date !== ""
       ) {
-        let temp = this.newExpense.category;
-        if (temp === this.categories[2]) {
-          temp = "matOchDryck";
-        } else if (temp === this.categories[4]) {
-          temp = "nöjeOchShopping";
-        }
-        temp = temp[0].toLowerCase() + temp.slice(1);
-        this.totalExpenses[temp].push({
-          price: this.newExpense.price,
-          date: this.newExpense.date,
-          desc: this.newExpense.desc,
-          category: this.newExpense.category,
-        });
-        this.totalExpenses.all.push({
+        this.expenses.push({
           price: this.newExpense.price,
           date: this.newExpense.date,
           desc: this.newExpense.desc,
@@ -61,64 +40,64 @@ let app = Vue.createApp({
         alert("Ej korrekt input");
       }
     },
+    removeItem(id) {
+      this.expenses.splice(id, 1);
+    },
   },
 });
 
 app.component("result-table", {
-  props: ["categories", "totalExp", "selectedCategory"],
+  props: ["categories", "allExpenses", "selectedCategory", "remove"],
   template: `
-    <div>
-      <label for="selectCategory">Select a category:</label>
-      <select id="selectCategory" v-model="selectedCategory">
-        <option value="">None</option>
-        <option value="all">All</option>
-        <option
-          v-for="(category, index) in categories"
-          :key="index"
-          :value="category"
-        >
-          {{category}}
-        </option>
-      </select>
-
-      <table v-if="selectedCategory !== ''">
-        <tr>
-          <th>Kostnad</th>
-          <th>Datum</th>
-          <th>Kategori</th>
-          <th>Beskrivning</th>
-        </tr>
-        <tr
-          v-for="(exp, index) in totalExp[formattedCategory]"
-          :key="index"
-        >
-          <td>{{exp.price}}</td>
-          <td>{{exp.date}}</td>
-          <td>{{exp.category}}</td>
-          <td>{{exp.desc}}</td>
-        </tr>
-        <tr>
-          <td>Summa:</td>
-          <td>{{sum}}</td>
-        </tr>
-      </table>
-    </div>
-  `,
+      <div>
+        <label for="selectCategory">Select a category:</label>
+        <select id="selectCategory" v-model="selectedCategory">
+          <option value="">None</option>
+          <option value="all">All</option>
+          <option
+            v-for="(category, index) in categories"
+            :key="index"
+            :value="category"
+          >
+            {{category}}
+          </option>
+        </select>
+  
+        <table v-if="selectedCategory !== ''">
+          <tr>
+            <th>Kostnad</th>
+            <th>Datum</th>
+            <th>Kategori</th>
+            <th>Beskrivning</th>
+          </tr>
+          <template v-for="(exp, index) in allExpenses"
+          :key="index"> 
+          <tr v-if="exp.category === selectedCategory || selectedCategory === 'all'">
+            <td>{{exp.price}}</td>
+            <td>{{exp.date}}</td>
+            <td>{{exp.category}}</td>
+            <td>{{exp.desc}}</td>
+            <button @click="remove(index)" class="far fa-trash-alt"></button>
+          </tr>
+          </template>
+          <tr>
+            <td>Summa:</td>
+            <td>{{sum}}</td>
+          </tr>
+        </table>
+      </div>
+    `,
   computed: {
-    formattedCategory() {
-      let temp = this.selectedCategory;
-      if (temp === this.categories[2]) {
-        temp = "matOchDryck";
-      } else if (temp === this.categories[4]) {
-        temp = "nöjeOchShopping";
-      }
-      return temp[0].toLowerCase() + temp.slice(1);
-    },
     sum() {
       let sum = 0;
-      if (this.formattedCategory !== "") {
-        this.totalExp[this.formattedCategory].forEach((exp) => {
-          sum += exp.price;
+      if (this.selectedCategory !== "") {
+        this.allExpenses.forEach((exp) => {
+          if (
+            exp.category === this.selectedCategory ||
+            this.selectedCategory === "all"
+          ) {
+            sum += exp.price;
+          }
         });
         return sum;
       }
