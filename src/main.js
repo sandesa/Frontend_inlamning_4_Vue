36a07647ab25a8d47e0f8509088413de939cb62f
Sampe <samuel.sandenas123@gmail.com>
2024-03-13 02:45:48 +0100

    Lade till sortering för kategori, kostnad och datum. Lade även till decimaler för kostnad

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 let app = Vue.createApp({
   data() {
     return {
+      descPrice: false,
+      descDate: false,
+      descCat: false,
       categories: [
         "Bostad",
         "Hushåll",
@@ -77,7 +80,7 @@ let app = Vue.createApp({
         this.newExpense.date !== ""
       ) {
         this.expenses.push({
-          price: this.newExpense.price.toFixed(2),
+          price: Math.round(this.newExpense.price * 100) / 100,
           date: this.newExpense.date,
           desc: this.newExpense.desc,
           category: this.newExpense.category,
@@ -93,11 +96,52 @@ let app = Vue.createApp({
     removeItem(index) {
       this.expenses.splice(index, 1);
     },
+    sortOnCategory() {
+      this.descCat = !this.descCat;
+      this.expenses = this.expenses.sort((a, b) => {
+        let expA = a.category.toLowerCase(),
+          expB = b.category.toLowerCase();
+        if (expA < expB) {
+          return this.descCat ? -1 : 1;
+        } else if (expA > expB) {
+          return this.descCat ? 1 : -1;
+        }
+        return 0;
+      });
+    },
+    sortOnPrice() {
+      this.descPrice = !this.descPrice;
+      this.expenses = this.expenses.sort((a, b) => {
+        let expA = a.price,
+          expB = b.price;
+        if (expA > expB) {
+          return this.descPrice ? -1 : 1;
+        } else if (expA < expB) {
+          return this.descPrice ? 1 : -1;
+        }
+        return 0;
+      });
+    },
+    sortOnDate() {
+      this.descDate = !this.descDate;
+      this.expenses = this.expenses.sort((a, b) => {
+        let dateA = new Date(a.date);
+        let dateB = new Date(b.date);
+        return this.descDate ? dateB - dateA : dateA - dateB;
+      });
+    },
   },
 });
 
 app.component("result-table", {
-  props: ["categories", "allExpenses", "selectedCategory", "remove"],
+  props: [
+    "categories",
+    "allExpenses",
+    "remove",
+    "sortCat",
+    "sortPrice",
+    "sortDate",
+  ],
   template: `
       <div>
         <label for="selectCategory">Select a category:</label>
@@ -115,9 +159,9 @@ app.component("result-table", {
   
         <table v-if="selectedCategory !== ''">
           <tr>
-            <th>Kostnad</th>
-            <th>Datum</th>
-            <th>Kategori</th>
+            <th><a href="#" @click="sortPrice">Kostnad</a></th>
+            <th><a href="#" @click="sortDate">Datum</a></th>
+            <th><a href="#" @click="sortCat">Category</a></th>
             <th>Beskrivning</th>
           </tr>
           <template v-for="(exp, index) in allExpenses"
@@ -148,8 +192,9 @@ app.component("result-table", {
           ) {
             sum += exp.price;
           }
+          sum = Math.round(sum * 100) / 100;
         });
-        return sum.toFixed(2);
+        return sum;
       }
     },
   },
@@ -158,6 +203,7 @@ app.component("result-table", {
       selectedCategory: "",
     };
   },
+  methods: {},
 });
 
 app.mount("#app");
