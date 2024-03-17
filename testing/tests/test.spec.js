fabb76fabe6cac1acc7acb4cdb23d5f36e02c2c1
Sampe <samuel.sandenas123@gmail.com>
2024-03-17 21:23:21 +0100

    data.json-strul

--- testing/tests/test.spec.js
+++ testing/tests/test.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@playwright/test");
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("http://127.0.0.1:5501/src/index.html");
+  await page.goto("http://127.0.0.1:5501/index.html");
 });
 
 test("Make sure the testing works:", async ({ page }) => {
